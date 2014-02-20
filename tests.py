c839adcb1eb2a884fe1b71305e0c03e6261787a4
Aaron O'Leary <eeaol@leeds.ac.uk>
2014-02-20 16:47:26 +0000

    simple integration test

---
+++ tests.py
@@ -0,0 +1,99 @@
+import notedown
+
+sample_markdown = u"""### Create IPython Notebooks from markdown
+
+This is a simple tool to convert markdown with code into an IPython
+Notebook.
+
+Usage:
+
+```
+notedown input.md > output.ipynb
+```
+
+
+It is really simple and separates your markdown into code and not
+code. Code goes into code cells, not-code goes into markdown cells.
+
+Installation:
+
+```
+pip install notedown
+```"""
+
+# Generate the sample notebook from the markdown using
+#
+#    import notedown
+#    reader = notedown.MarkdownReader()
+#    sample_notebook = reader.reads(sample_markdown)
+#    writer = notedown.JSONWriter()
+#    print writer.writes(sample_notebook)
+#
+sample_notebook = r"""{
+ "metadata": {},
+ "nbformat": 3,
+ "nbformat_minor": 0,
+ "worksheets": [
+  {
+   "cells": [
+    {
+     "cell_type": "markdown",
+     "metadata": {},
+     "source": [
+      "### Create IPython Notebooks from markdown\n",
+      "\n",
+      "This is a simple tool to convert markdown with code into an IPython\n",
+      "Notebook.\n",
+      "\n",
+      "Usage:"
+     ]
+    },
+    {
+     "cell_type": "code",
+     "collapsed": false,
+     "input": [
+      "notedown input.md > output.ipynb"
+     ],
+     "language": "python",
+     "metadata": {},
+     "outputs": []
+    },
+    {
+     "cell_type": "markdown",
+     "metadata": {},
+     "source": [
+      "It is really simple and separates your markdown into code and not\n",
+      "code. Code goes into code cells, not-code goes into markdown cells.\n",
+      "\n",
+      "Installation:"
+     ]
+    },
+    {
+     "cell_type": "code",
+     "collapsed": false,
+     "input": [
+      "pip install notedown"
+     ],
+     "language": "python",
+     "metadata": {},
+     "outputs": []
+    }
+   ],
+   "metadata": {}
+  }
+ ]
+}"""
+
+
+def create_json_notebook():
+    reader = notedown.MarkdownReader()
+    writer = notedown.JSONWriter()
+
+    notebook = reader.reads(sample_markdown)
+    json_notebook = writer.writes(notebook)
+    return json_notebook
+
+
+def test_notedown():
+    """Integration test the whole thing."""
+    assert(create_json_notebook() == sample_notebook)
