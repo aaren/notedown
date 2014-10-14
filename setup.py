7712e390fbdd1b88ced25fa01ced9d84c6d47823
Aaron O'Leary <aaron.oleary@gmail.com>
2014-10-14 13:07:02 +0100

    only require ipython, jinja2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author_email='dev@aaren.me',
     license='BSD 2-Clause',
     url='http://github.com/aaren/notedown',
-    install_requires=['ipython[all]', ],
+    install_requires=['ipython', 'jinja2'],
     entry_points={'console_scripts': ['notedown = notedown:cli', ]},
     package_dir={'notedown': 'notedown'},
     package_data={'notedown': ['templates/markdown.tpl']},
