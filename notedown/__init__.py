c03bda6453c4eb8ae58867a7c70177818db9e50e
Aaron O'Leary <eeaol@leeds.ac.uk>
2015-04-28 16:55:11 +0100

    add a simple convert method

--- notedown/__init__.py
+++ notedown/__init__.py
@@ -1,3 +1,27 @@
 from __future__ import absolute_import
 from .notedown import *
 from .main import markdown_template, __version__
+
+def convert(content, informat, outformat):
+    if os.path.exists(content):
+        with open(content) as f:
+            contents = f.read()
+    else:
+        contents = content
+
+    readers = {'notebook': nbformat,
+               'markdown': MarkdownReader(precode='',
+                                          magic=False,
+                                          match='fenced')
+               }
+
+    writers = {'notebook': nbformat,
+               'markdown': MarkdownWriter(markdown_template,
+                                          strip_outputs=False)
+               }
+
+    reader = readers[informat]
+    writer = writers[outformat]
+
+    notebook = reader.reads(contents, as_version=4)
+    return writer.writes(notebook)
