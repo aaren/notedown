441c2df44fe563a2463a5c188b6ea4fef51a4815
Laurent Gautier <lgautier@gmail.com>
2015-09-07 15:24:52 -0400

    Update setup.py
    Preventing install with IPython 4 is possibly in way of letting people propose patches for IPython 4.0.0.

--- setup.py
+++ setup.py
@@ -13,6 +13,19 @@ except OSError:
     with open('README.md') as f:
         readme = f.read()
 
+# Issue a warning if IPython >= 4
+import warnings
+import IPython
+ip_version = IPython.__version__.split('.')
+try:
+    ip_major = int(ip_version[0])
+except:
+    ip_major = None
+if ip_major is None:
+    warnings.warn('The version number of IPython cannot be determined.')
+elif ip_major >= 4:
+    warnings.warn('Your IPython version is %s while notedown is designed against IPython > 3.0 but < 4.0.' % IPython.__version__)
+
 setup(
     name="notedown",
     version="1.4.5",
@@ -24,7 +37,7 @@ setup(
     license='BSD 2-Clause',
     url='http://github.com/aaren/notedown',
     # conda gets confused by ipython[nbconvert] so be explicit..
-    install_requires=['ipython >= 3.0, < 4.0',
+    install_requires=['ipython >= 3.0',
                       'jinja2',
                       'mistune',
                       'pygments',
