885aec52bfbd54767056bc1d90b1b74cfd8c6a93
Aaron O'Leary <eeaol@leeds.ac.uk>
2015-10-07 17:48:51 +0100

    release 1.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except OSError:
 
 setup(
     name="notedown",
-    version="1.4.6",
+    version="1.5.0",
     description="Convert markdown to IPython notebook.",
     long_description=readme,
     packages=['notedown'],
